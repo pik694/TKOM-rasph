3a1e624ddad5b3e3d67ea623eb3d2974e7bde7cb
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-04-16 20:36:39 +0200

    Add lexer module

---
+++ src/lexer/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Piotr Żelazko on 16.04.2018.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    std::cout << "Hello world!" << std::endl;
+}
---
+++ tests/lexer_tests/sample_tests.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Piotr Żelazko on 16.04.2018.
+//
+#define BOOST_TEST_MODULE lexer_tests
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(lexer_test_suite)
+
+    BOOST_AUTO_TEST_CASE(sample_test_case) {
+        BOOST_CHECK_EQUAL(1,1);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
