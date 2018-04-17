91170dff96d61b709dc5148d9199436a57f7288f
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-04-17 12:50:59 +0200

    Change Boost.Test usage
    It now uses Boost.Test framework as a dynlib

--- tests/lexer_tests/sample_tests.cpp
+++ tests/lexer_tests/sample_tests.cpp
@@ -1,12 +1,15 @@
 //
 // Created by Piotr Żelazko on 16.04.2018.
 //
-#define BOOST_TEST_MODULE lexer_tests
-#include <boost/test/included/unit_test.hpp>
+
+#include <boost/test/unit_test.hpp>
+#define BOOST_TEST_DYN_LINK
+
 
 BOOST_AUTO_TEST_SUITE(lexer_test_suite)
 
     BOOST_AUTO_TEST_CASE(sample_test_case) {
+        new int;
         BOOST_CHECK_EQUAL(1,1);
     }
 
---
+++ tests/tests_main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Piotr Żelazko on 17.04.2018.
+//
+
+
+#define BOOST_TEST_MODULE rasph tests
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
