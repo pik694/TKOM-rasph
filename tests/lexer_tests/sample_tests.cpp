97b151f093ca50762c8ee9041d197ad18dabc254
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-04-18 00:10:43 +0200

    Minor changes in sample tests

--- tests/lexer_tests/sample_tests.cpp
+++ tests/lexer_tests/sample_tests.cpp
@@ -5,14 +5,26 @@
 #include <boost/test/unit_test.hpp>
 #define BOOST_TEST_DYN_LINK
 
+#ifdef BOOST_TEST_DYN_LINK
+
+#include "lexer/Lexer.h"
 
 BOOST_AUTO_TEST_SUITE(lexer_test_suite)
 
     BOOST_AUTO_TEST_CASE(sample_test_case) {
-        new int;
+
+        rasph::lexer::Lexer lexer;
         BOOST_CHECK_EQUAL(1,1);
+        
+        BOOST_CHECK_EQUAL(lexer.a(),10);
+    }
+    BOOST_AUTO_TEST_CASE(sample_test_case1) {
+
+        rasph::lexer::Lexer lexer;
+
+        BOOST_CHECK_EQUAL(lexer.a(),10);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
 
-
+#endif // BOOST_TEST_DYN_LINK
