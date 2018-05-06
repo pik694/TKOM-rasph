40d96a1d4d83dea9b7d5fd6618a2d3da132efcba
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-05-06 21:42:33 +0200

    Add parser module

---
+++ src/parser/Parser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Piotr Żelazko on 06.05.2018.
+//
+
+#include "Parser.hpp"
---
+++ src/parser/Parser.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Piotr Żelazko on 06.05.2018.
+//
+
+#ifndef RASPH_PARSER_HPP
+#define RASPH_PARSER_HPP
+
+namespace rasph::parser {
+    class Parser {
+
+    };
+}
+
+#endif //RASPH_PARSER_HPP
